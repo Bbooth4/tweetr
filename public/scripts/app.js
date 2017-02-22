e9064dcf6a0f7955ea39d18fa616ae4089996895
Brandon <booth.brandon4@gmail.com>
2017-02-22 19:46:39 +0000

    All done with the functionality!

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,61 +1,4 @@
-var data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
-
 $(() => {
-  function loadTweets(tweet) {
-    $.ajax({
-      url: '/tweets',
-      method: 'POST',
-      data: $('form').serialize(),
-      success: function(tweet) {
-        console.log($('#tweet-wrapper').prepend(tweet));
-      }
-    });
-  }
 
   function createTweetElement(tweet) {
     const personName = tweet.user.name;
@@ -73,20 +16,56 @@ $(() => {
         <div class='content' >${content}</div>
         <footer>
           <p class='days-ago'>${daysAgo}</p>
+          <div class='flags'>
+              <i class="fa fa-flag" aria-hidden="true"></i>
+              <i class="fa fa-retweet" aria-hidden="true"></i>
+              <i class="fa fa-heart" aria-hidden="true"></i>
+          </div>
         </footer>
       </article>
       `
     return $tweet;
   }
 
+  function loadTweets() {
+    $.ajax({
+      method: 'GET',
+      url: '/tweets'
+    }).done((data) => {
+      renderTweets(data);
+    }).fail(console.error);
+  }; 
+
+
   function renderTweets(tweets) {
-    return tweets.forEach(tweet => $('.tweet-wrapper').prepend(createTweetElement(tweet)) ); 
+    $('.tweet-wrapper').empty();
+    return tweets.map(tweet => $('.tweet-wrapper').prepend(createTweetElement(tweet)) ); 
   }
 
-  $('input').click(function(event) {
+  loadTweets();
+
+  $('form').submit((event) => {
     event.preventDefault();
-    const data = $('#create-tweet')[0].value;
-    loadTweets(data);
+    const newTweet = $(event.target).serialize();
+    if ($('#create-tweet').val().length === 0 || $('#create-tweet').val().length === '') {
+      alert('You did not input any information!');
+    } else if ($('#create-tweet').val().length > 140) {
+      alert('Your tweet has too many characters!');
+    } else {
+      $.ajax({
+        method: 'POST',
+        url: '/tweets',
+        data: newTweet,
+      }).done((result) => {
+        loadTweets();
+        $('#create-tweet').val(''); 
+        $('.counter').text(140);
+      }).fail((error) => console.error(error));
+    }
   });
-  renderTweets(data);
+  $('.new-tweet').slideUp(0, function(){}); 
+  $('#compose').click((event) => {
+    $('.new-tweet').slideToggle('slow', function(){});
+    $('#create-tweet').focus();
+  });
 });
