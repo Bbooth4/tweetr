b24b7b9d51a40dfb60e154cb4f9e18001f77f9ab
Brandon <booth.brandon4@gmail.com>
2017-02-23 20:38:34 +0000

    mongo implement sucessfully

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,43 +1,29 @@
 "use strict";
 
-// Simulates the kind of delay we see with network or filesystem operations
 const simulateDelay = require("./util/simulate-delay");
 
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
   return {
 
-    // Saves a tweet to `db` 
-    saveTweet: function(newTweet, callback) {
-      // simulateDelay(() => {
-      db.collection('tweets').insertOne(tweet, (err, tweets) => {
-        if (err) {
-          console.log('Failed to retreive tweets.');
-          return callback('Failed to retreive tweets.');
-          // db.tweets.push(newTweet);
-        } else {
-          callback(null, true);
-        }
-      });
-    },
-
-    // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      // simulateDelay(() => {
-        db.collection("tweets").find().toArray((err, tweets) => {
+      db.collection("tweets").find().toArray((err, tweets) => {
           if (err) {
             console.log('Failed to retreive tweets.');
             return callback(err);
           } else {
-          // callback(null, tweets);
-            // const sortNewestFirst = (a, b) => a.created_at - b.created_at;
             callback(null, tweets);
           }
         });
-    }
+      },
+      saveTweet: function(newTweet, callback) {
+        db.collection("tweets").insertOne(newTweet, (err, result) => {
+         if (err) {
+           console.log('Failed to load tweets.');
+           return callback(err);
+         } 
+         callback(null, true);
+        });
+      }
   }
-  // return {
-  //   saveTweet: saveTweet, 
-  //   getTweets: getTweets
-  // }
 }
